24697ab8639360b71ef2d35eac3259e37dc7cb03
Alex <alex@Air-de-Melanie.lan>
2025-07-21 13:05:23 +0200

    Fix: ajout déclaration de type global pour html2pdf.js pour résoudre l'erreur TypeScript sur Vercel

---
+++ client/src/types/global.d.ts
@@ -0,0 +1,23 @@
+declare module 'html2pdf.js' {
+  interface Html2PdfOptions {
+    margin?: number | [number, number, number, number];
+    filename?: string;
+    image?: { type?: string; quality?: number };
+    enableLinks?: boolean;
+    html2canvas?: any;
+    jsPDF?: any;
+  }
+
+  interface Html2PdfInstance {
+    from(element: HTMLElement): Html2PdfInstance;
+    set(options: Html2PdfOptions): Html2PdfInstance;
+    save(): Promise<void>;
+    outputPdf(): Promise<Blob>;
+    outputImg(): Promise<string>;
+  }
+
+  function html2pdf(): Html2PdfInstance;
+  function html2pdf(element: HTMLElement, options?: Html2PdfOptions): Html2PdfInstance;
+
+  export = html2pdf;
+} 
