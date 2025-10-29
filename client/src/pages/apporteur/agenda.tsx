985d66da2c06fd964c253e3a6faaa2396fe07789
Alex <alex@Air-de-Alex.lan>
2025-10-29 20:06:11 +0100

    feat: utilisation du UnifiedCalendar pour expert et apporteur agenda

--- client/src/pages/apporteur/agenda.tsx
+++ client/src/pages/apporteur/agenda.tsx
@@ -1,13 +1,20 @@
-import { UnifiedAgendaView } from '../../components/rdv/UnifiedAgendaView';
+import { UnifiedCalendar } from '../../components/UnifiedCalendar';
 
 /**
  * Page Agenda Apporteur
- * Utilise le composant UnifiedAgendaView pour affichage multi-types
+ * Utilise le composant UnifiedCalendar pour affichage multi-types
  */
 export default function AgendaPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 p-6">
-      <UnifiedAgendaView />
+      <div className="max-w-7xl mx-auto">
+        <UnifiedCalendar 
+          showHeader={true}
+          showViewSelector={true}
+          enableRealTime={true}
+          defaultView="week"
+        />
+      </div>
     </div>
   );
 }
--- client/src/pages/expert/agenda.tsx
+++ client/src/pages/expert/agenda.tsx
@@ -1,13 +1,20 @@
-import { UnifiedAgendaView } from '../../components/rdv/UnifiedAgendaView';
+import { UnifiedCalendar } from '../../components/UnifiedCalendar';
 
 /**
  * Page Agenda Expert
- * Utilise le composant UnifiedAgendaView pour affichage multi-types
+ * Utilise le composant UnifiedCalendar pour affichage multi-types
  */
 export default function AgendaExpert() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-50 to-teal-100 p-6">
-      <UnifiedAgendaView />
+      <div className="max-w-7xl mx-auto">
+        <UnifiedCalendar 
+          showHeader={true}
+          showViewSelector={true}
+          enableRealTime={true}
+          defaultView="week"
+        />
+      </div>
     </div>
   );
 }
